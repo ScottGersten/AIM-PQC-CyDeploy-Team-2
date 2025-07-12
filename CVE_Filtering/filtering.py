7bddd455a92a94303f45c9207417737fb1376ce1
ScottGersten <130173316+ScottGersten@users.noreply.github.com>
2025-07-12 01:58:36 -0400

    Basic scan and parsing logic for NMAP result

--- CVE_Filtering/filtering.py
+++ CVE_Filtering/filtering.py
@@ -1,2 +1,30 @@
-# testing git
-# testing git branch
+import json
+from nmap import PortScanner
+import socket
+from packaging.version import Version, InvalidVersion
+
+def scan_ip(target: str, start=1, end=1024):
+    ip = socket.gethostbyname(target)
+    print(f"Target IP: {ip}")
+    scanner = PortScanner()
+    result = scanner.scan(ip, f"{start}-{end}", '-sV')
+    protocols = result['scan'][ip].all_protocols()
+    services = []
+
+    #rint(scanner[ip].all_protocols())
+    #print(result)
+    #print(protocols)
+
+    for protocol in protocols:
+        for port in result['scan'][ip][protocol]:
+            service = result['scan'][ip][protocol][port]
+            services.append({
+                'port': port,
+                'name': service.get('name'),
+                'product': service.get('product'),
+                'version': service.get('version')
+            })
+
+    return services
+
+print(scan_ip('scanme.nmap.org'))
