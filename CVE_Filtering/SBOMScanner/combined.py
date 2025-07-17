04dc47343c38709260245e358d5808984c7282a3
ScottGersten <130173316+ScottGersten@users.noreply.github.com>
2025-07-17 00:43:21 -0400

    Code and result cleanup

--- CVE_Filtering/SBOMScanner/combined.py
+++ CVE_Filtering/SBOMScanner/combined.py
@@ -2,6 +2,7 @@ import paramiko
 import requests
 import json
 from concurrent.futures import ThreadPoolExecutor, as_completed
+import time
 
 def get_debian_tracker():
     url = "https://security-tracker.debian.org/tracker/data/json"
@@ -19,46 +20,15 @@ def get_debian_cves(data, pkg):
     for cve in pkg_data.items():
         cves.append(cve[0])
     
-    return cves
+    return sorted(cves)
 
 def debian_method(installs):
-    # Debian Tracker Method
-    debian_fails = debian_successes = 0
-
     data = get_debian_tracker()
 
     for pkg in installs:
         cves = get_debian_cves(data, pkg['name'])
         if cves is not None:
             pkg['cves'].append(cves)
-
-    # for pkg in installs[30:40]:
-    #     cves = get_cves(data, pkg['name'])
-    #     pkg['cves'] = cves
-    #     print(cves)
-
-    # print(installs[30:40])
-
-    # with open('results.json', 'w', encoding='utf-8') as file:
-    #     json.dump(installs, file, indent=2)
-
-    # for pkg in installs:
-    #     if not pkg['cves']:
-    #         debian_fails += 1
-    #     else:
-    #         debian_successes += 1
-
-    # print(f"Number of successful matches in Debian: {debian_successes}")
-    # print(f"Number of failed matches in Debian: {debian_fails}")
-
-def osv_threading(pkgs, max_threads=100):
-    with ThreadPoolExecutor(max_workers=max_threads) as executor:
-        futures = {executor.submit(get_osv_cves, pkg['name']): pkg for pkg in pkgs}
-        for future in as_completed(futures):
-            pkg = futures[future]
-            cves = future.result()
-            if cves is not None:
-                pkg['cves'].append(cves)
 
 def get_osv_cves(pkg, ecosystem='Debian'):
 
@@ -78,44 +48,28 @@ def get_osv_cves(pkg, ecosystem='Debian'):
         vulns = results.get('vulns', [])
         cves = []
         for vuln in vulns:
-            if 'aliases' in vuln:
-                cves.extend(vuln['aliases'])
-            else:
-                cves.append(vuln.get('id'))
-        #return list(set(cves))
+            # if 'aliases' in vuln:
+            #     cves.extend(vuln['aliases'])
+            # else:
+            #     cves.append(vuln.get('id'))
+            cves.append(vuln.get('id'))
         if cves:
-            #osv_successes += 1
-            return list(set(cves))
+            return sorted(list(set(cves)))
         else:
-            #osv_fails += 1
             return None
     
     except Exception as e:
-        #osv_fails += 1
         print(f"{e}")
         return None
-
-def osv_method(installs):
-    osv_fails =  osv_successes = 0
-
-    # OSV JSON Method
-    osv_threading(installs)
-
-    # for pkg in installs[30:40]:
-    #     cves = get_osv(pkg['name'])
-    #     pkg['cves'] = cves
-
-    # with open('results.json', 'w', encoding='utf-8') as file:
-    #     json.dump(installs, file, indent=2)
-
-    # for pkg in installs:
-    #     if not pkg['cves']:
-    #         osv_fails += 1
-    #     else:
-    #         osv_successes += 1
-
-    # print(f"Number of successful matches in OSV: {osv_successes}")
-    # print(f"Number of failed matches in OSV: {osv_fails}")
+    
+def osv_method(pkgs, max_threads=100):
+    with ThreadPoolExecutor(max_workers=max_threads) as executor:
+        futures = {executor.submit(get_osv_cves, pkg['name']): pkg for pkg in pkgs}
+        for future in as_completed(futures):
+            pkg = futures[future]
+            cves = future.result()
+            if cves is not None:
+                pkg['cves'].append(cves)
 
 def get_installs(ip, username='msfadmin', password='msfadmin'):
     ssh = paramiko.SSHClient()
@@ -147,12 +101,16 @@ def parse_installs(installs):
     return packages
 
 def main():
+    start_time = time.time()
+
     with open('ip.txt', 'r') as f:
         ip = f.read()
     #ip = '192.168.56.101'
     #output = get_installs(ip)
+
     with open ('installed.txt', 'r') as f:
         output = f.read()
+
     installs = parse_installs(output)
 
     debian_method(installs)
@@ -160,18 +118,26 @@ def main():
     osv_method(installs)
 
     full_fails = full_successes = 0
+    found_installs = []
 
     for pkg in installs:
         if not pkg['cves']:
             full_fails += 1
         else:
             full_successes += 1
+            found_installs.append(pkg)
 
     print(f"Number of successful matches in Full Run: {full_successes}")
     print(f"Number of failed matches in Full Run: {full_fails}")
 
     with open('results.json', 'w', encoding='utf-8') as file:
         json.dump(installs, file, indent=2)
+
+    with open('results_abridged.json', 'w', encoding='utf-8') as file:
+        json.dump(found_installs, file, indent=2)
+    
+    end_time = time.time() - start_time
+    print(f"Execution Time: {end_time:.4f}")
 
 if __name__ == '__main__':
     main()
