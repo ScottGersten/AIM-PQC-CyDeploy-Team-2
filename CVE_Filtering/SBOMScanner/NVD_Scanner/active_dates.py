350f5c165fd171117d1c5c95c0b81862ec3d783f
ScottGersten <130173316+ScottGersten@users.noreply.github.com>
2025-07-25 02:36:52 -0400

    First year to last year by date logic

--- CVE_Filtering/SBOMScanner/NVD_Scanner/active_dates.py
+++ CVE_Filtering/SBOMScanner/NVD_Scanner/active_dates.py
@@ -3,19 +3,57 @@ import re
 from datetime import datetime
 import time
 
-def get_package_year(ssh, pkg):
-    cmd = f"zgrep -m 1 -E '^ --' /usr/share/doc/{pkg}/changelog.Debian.gz"
+def get_years_from_changelog(ssh, pkg):
+    cmd = f"zgrep '^ --' /usr/share/doc/{pkg}/changelog.Debian.gz"
     try:
         stdin, stdout, stderr = ssh.exec_command(cmd)
-        output = stdout.read().decode().strip()
-        if output:
-            date_match = re.search(r'\w{3}, \d{1,2} \w{3} \d{4}', output)
+        lines = stdout.read().decode().strip().splitlines()
+
+        years = []
+        for line in lines:
+            date_match = re.search(r'\w{3}, \d{1,2} \w{3} \d{4}', line)
             if date_match:
-                date_str = date_match.group(0)
-                date_obj = datetime.strptime(date_str, "%a, %d %b %Y")
-                return date_obj.year
+                try:
+                    date_obj = datetime.strptime(date_match.group(0), "%a, %d %b %Y")
+                    years.append(date_obj.year)
+                except ValueError:
+                    continue
+
+        if years:
+            return years[-1], years[0]
+        else:
+            return None, None
     except Exception as e:
-        return None
+        return None, None
+
+# def get_first_year(ssh, pkg):
+#     cmd = f"zgrep '^ --' /usr/share/doc/{pkg}/changelog.Debian.gz | tail -1"
+#     try:
+#         stdin, stdout, stderr = ssh.exec_command(cmd)
+#         output = stdout.read().decode().strip()
+#         if output:
+#             date_match = re.search(r'\w{3}, \d{1,2} \w{3} \d{4}', output)
+#             if date_match:
+#                 date_str = date_match.group(0)
+#                 date_obj = datetime.strptime(date_str, "%a, %d %b %Y")
+#                 return date_obj.year
+#     except Exception as e:
+#         return None
+
+# def get_last_year(ssh, pkg):
+#     #cmd = f"zgrep -m 1 -E '^ --' /usr/share/doc/{pkg}/changelog.Debian.gz"
+#     cmd = f"zgrep -E '^ --' /usr/share/doc/{pkg}/changelog.Debian.gz | head -1"
+#     try:
+#         stdin, stdout, stderr = ssh.exec_command(cmd)
+#         output = stdout.read().decode().strip()
+#         if output:
+#             date_match = re.search(r'\w{3}, \d{1,2} \w{3} \d{4}', output)
+#             if date_match:
+#                 date_str = date_match.group(0)
+#                 date_obj = datetime.strptime(date_str, "%a, %d %b %Y")
+#                 return date_obj.year
+#     except Exception as e:
+#         return None
     
 def get_years(ip, username='msfadmin', password='msfadmin'):
     ssh = paramiko.SSHClient()
@@ -25,17 +63,26 @@ def get_years(ip, username='msfadmin', password='msfadmin'):
     with open('installed.txt', 'r') as file:
         installs = file.read()
 
-    release_years = {}
+    first_years = {}
+    last_years = {}
+    count = 0
     for line in installs.splitlines():
         if line.startswith('ii'):
+            # count += 1
+            # if count > 100:
+            #     break
             splits = line.split()
             pkg_name = splits[1]
-            year = get_package_year(ssh, pkg_name)
-            if year:
-                release_years[pkg_name] = year
+            first_year, last_year = get_years_from_changelog(ssh, pkg_name)
+            #first_year = get_first_year(ssh, pkg_name)
+            if first_year:
+                first_years[pkg_name] = first_year
+            #last_year = get_last_year(ssh, pkg_name)
+            if last_year:
+                last_years[pkg_name] = last_year
     
     ssh.close()
-    return release_years
+    return first_years, last_years
 
 def main():
     start_time = time.time()
@@ -43,10 +90,14 @@ def main():
     with open('ip.txt', 'r') as file:
         ip = file.read()
     
-    #release_years = len(get_years(ip))
-    release_years = get_years(ip)
-    
+    release_years, end_years = get_years(ip)
+    num_release = len(release_years)
+    num_end = len(end_years)
+
     print(release_years)
+    print(end_years)
+    print(f"Release Years Found: {num_release}")
+    print(f"End Years Found: {num_end}")
 
     end_time = time.time() - start_time
     print(f"Execution Time: {end_time:.4f}")
--- CVE_Filtering/SBOMScanner/NVD_Scanner/nvd_method_by_date.py
+++ CVE_Filtering/SBOMScanner/NVD_Scanner/nvd_method_by_date.py
@@ -8,16 +8,17 @@ import threading
 
 found_cve_ids = 0
 
-def match_cves(installs, data, year_offset=5):
+def match_cves(installs, data):
     global found_cve_ids
 
     for pkg in installs:
         name = pkg['name']
-        release_year = pkg['release_year']
-        if release_year is None:
+        first_year = pkg['first_year']
+        last_year = pkg['last_year']
+        if first_year is None or last_year is None:
             continue
         
-        for year in range(release_year, release_year + year_offset + 1):
+        for year in range(first_year, last_year + 1):
             year_str = str(year)
             if year_str not in data:
                 continue
@@ -28,19 +29,82 @@ def match_cves(installs, data, year_offset=5):
                     pkg['cves'].append(cve_id)
                     found_cve_ids += 1
 
-def get_package_year(ssh, pkg):
-    cmd = f"zgrep -m 1 -E '^ --' /usr/share/doc/{pkg}/changelog.Debian.gz"
+# def match_cves(installs, data, present_year=2025):
+#     global found_cve_ids
+
+#     for pkg in installs:
+#         name = pkg['name']
+#         release_year = pkg['release_year']
+#         if release_year is None:
+#             continue
+        
+#         for year in range(release_year, present_year + 1):
+#             year_str = str(year)
+#             if year_str not in data:
+#                 continue
+#             for item in data[year_str]:
+#                 cve_id = item.get('id')
+#                 description = item.get('description', '')
+#                 if name.lower() in description.lower():
+#                     pkg['cves'].append(cve_id)
+#                     found_cve_ids += 1
+
+# def match_cves(installs, data, year_offset=5):
+#     global found_cve_ids
+
+#     for pkg in installs:
+#         name = pkg['name']
+#         release_year = pkg['release_year']
+#         if release_year is None:
+#             continue
+        
+#         for year in range(release_year, release_year + year_offset + 1):
+#             year_str = str(year)
+#             if year_str not in data:
+#                 continue
+#             for item in data[year_str]:
+#                 cve_id = item.get('id')
+#                 description = item.get('description', '')
+#                 if name.lower() in description.lower():
+#                     pkg['cves'].append(cve_id)
+#                     found_cve_ids += 1
+
+# def get_package_year(ssh, pkg):
+#     cmd = f"zgrep -m 1 -E '^ --' /usr/share/doc/{pkg}/changelog.Debian.gz"
+#     try:
+#         stdin, stdout, stderr = ssh.exec_command(cmd)
+#         output = stdout.read().decode().strip()
+#         if output:
+#             date_match = re.search(r'\w{3}, \d{1,2} \w{3} \d{4}', output)
+#             if date_match:
+#                 date_str = date_match.group(0)
+#                 date_obj = datetime.strptime(date_str, "%a, %d %b %Y")
+#                 return date_obj.year
+#     except Exception as e:
+#         return None
+
+def get_package_years(ssh, pkg):
+    cmd = f"zgrep '^ --' /usr/share/doc/{pkg}/changelog.Debian.gz"
     try:
         stdin, stdout, stderr = ssh.exec_command(cmd)
-        output = stdout.read().decode().strip()
-        if output:
-            date_match = re.search(r'\w{3}, \d{1,2} \w{3} \d{4}', output)
+        lines = stdout.read().decode().strip().splitlines()
+
+        years = []
+        for line in lines:
+            date_match = re.search(r'\w{3}, \d{1,2} \w{3} \d{4}', line)
             if date_match:
-                date_str = date_match.group(0)
-                date_obj = datetime.strptime(date_str, "%a, %d %b %Y")
-                return date_obj.year
+                try:
+                    date_obj = datetime.strptime(date_match.group(0), "%a, %d %b %Y")
+                    years.append(date_obj.year)
+                except ValueError:
+                    continue
+
+        if years:
+            return years[-1], years[0]
+        else:
+            return None, None
     except Exception as e:
-        return None
+        return None, None
 
 def get_installs(ip, username='msfadmin', password='msfadmin'):
     ssh = paramiko.SSHClient()
@@ -58,11 +122,14 @@ def get_installs(ip, username='msfadmin', password='msfadmin'):
     for line in output.splitlines():
         if line.startswith('ii'):
             splits = line.split()
+            first_year, last_year = get_package_years(ssh, splits[1])
             packages.append({
                 'name': splits[1],
                 'version': splits[2],
                 #'description': ''.join(splits[3:]),
-                'release_year': get_package_year(ssh, splits[1]),
+                #'release_year': get_package_year(ssh, splits[1]),
+                'first_year': first_year,
+                'last_year': last_year,
                 'cves': []
             })
 
@@ -77,9 +144,9 @@ def main():
 
     with open('ip.txt', 'r') as f:
         ip = f.read()
-    installs = get_installs(ip)
-    # with open('installed.json', 'r', encoding='utf-8') as file:
-    #     installs = json.load(file)
+    #installs = get_installs(ip)
+    with open('installed.json', 'r', encoding='utf-8') as file:
+        installs = json.load(file)
 
     with open('all_cves_by_date.json', 'r', encoding='utf-8') as file:
         all_cves = json.load(file)
